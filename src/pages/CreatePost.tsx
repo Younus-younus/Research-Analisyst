12212439e1b517ac88c6a3ce9380d984468aafbc
Younus <you@example.com>
2025-03-13 17:45:21 +0530

    changes 5

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -11,7 +11,7 @@ const CreatePost = () => {
   const [category, setCategory] = useState('');
   const [error, setError] = useState('');
   const { isLoggedIn } = useContext(AuthContext);
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:1000";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
